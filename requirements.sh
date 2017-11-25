734fad63539f6edccb486e4d8f48d52fa567d106
Peter Rijnbeek <p.rijnbeek@erasmusmc.nl>
2017-11-25 22:16:41 +0100

    check sql

--- requirements.sh
+++ requirements.sh
@@ -3,11 +3,11 @@ REPO_PATH=VocabularyCourse
 
 pip3 install sqlwhat-ext==0.0.1
 
-apt-get update && apt-get install -y unzip
+#apt-get update && apt-get install -y unzip
 
-mkdir -p $REPO_PATH/data
+#mkdir -p $REPO_PATH/data
 # You can replace this with a link to your archive of csv files
-wget "https://assets.datacamp.com/course/sql/films.zip"
-unzip films.zip -d $REPO_PATH/data
+#wget "https://assets.datacamp.com/course/sql/films.zip"
+#unzip films.zip -d $REPO_PATH/data
 
 service postgresql start   && sudo -u postgres createdb -O repl films   && cd $REPO_PATH   && sudo -u postgres psql films < data/films/films.sql   && service postgresql stop
